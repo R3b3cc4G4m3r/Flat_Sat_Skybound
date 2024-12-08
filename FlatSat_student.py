d89912ebc735066ccd83c4e056e978a9c0ec465a
Flavia <flavia2006martinez@gmail.com>
2024-12-08 11:18:08 -0500

    added folder path FlatSat_student.py

--- FlatSat_student.py
+++ FlatSat_student.py
@@ -24,7 +24,7 @@ from picamera2 import Picamera2
 #VARIABLES
 THRESHOLD = 0      #Any desired value from the accelerometer
 REPO_PATH = ""     REPO_PATH = "/home/pi/Flat_Sat_Skybound" #Your github repo path: ex. /home/pi/FlatSatChallenge
-FOLDER_PATH = ""   #Your image folder path in your GitHub repo: ex. /Images
+FOLDER_PATH = ""   FOLDER_PATH = "Images" #Your image folder path in your GitHub repo: ex. /Images
 
 #imu and camera initialization
 i2c = board.I2C()
